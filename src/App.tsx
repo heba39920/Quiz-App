f37cf59b83d7a1534002865559a49d7d87066910
heba39920 <hbhsabr47@gmail.com>
2025-07-22 03:05:12 +0300

    folder structure and  depandencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-
+import { ToastContainer } from 'react-toastify';
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ChangePassword, Dashboard, ForgetPassword, Login, NotFound, Register, ResetPassword } from './pages';
+
 
 function App() {
 
 
+    const routes = createBrowserRouter(
+    [
+    
+      {
+        // path: "/",
+        // element: <AuthLayout />,
+        errorElement: <NotFound />,
+        children: [
+          { index: true, element: <Login /> },
+          { path: "login", element: <Login /> },
+          { path: "register", element: <Register /> },
+          { path: "forget-password", element: <ForgetPassword/> },
+          { path: "reset-password", element: <ResetPassword /> },
+          { path: "change-password", element: <ChangePassword /> },
+        ],
+      },
+      {
+        path: "/dashboard",
+        // element: <MasterLayout />,
+        errorElement: <NotFound />,
+        children: [
+          { index: true, element: <Dashboard /> },
+          { path: "dashboard", element: <Dashboard /> },
+        ],
+      },
+    ],
+    {
+      basename: "/Quiz-App/",
+    }
+  );
+
   return (
- <></>
-  )
+    <>
+      <ToastContainer position="top-center"  />
+      <RouterProvider router={routes} />
+    </>
+  );
 }
 
 export default App
---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+/*************Authentication*******************/
+export { default as Login } from "./Authentication/Login/Login";
+export { default as Register } from "./Authentication/Register/Register";
+export { default as ResetPassword } from "./Authentication/ResetPassword/ResetPassword";
+export { default as ForgetPassword } from "./Authentication/ForgetPassword/ForgetPassword";
+export { default as ChangePassword } from "./Authentication/ChangePassword/ChangePassword";
+export { default as NotFound } from "./NotFound/NotFound";
+export { default as Dashboard } from "./Dashboard/Dashboard";
+
+
